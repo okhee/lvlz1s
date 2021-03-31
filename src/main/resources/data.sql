52e2dc4d492984d029a87d75eb97675f5598c7c0
Jung Jongyoon <yoon97610@naver.com>
2021-03-31 21:34:28 +0900

    Add database initialization

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into song (album_id, file_id, song_name, uuid) values (122, 2151, 'Candy Jelly Love', 'UUID2134');
+insert into song (album_id, file_id, song_name, uuid) values (122, 1135, '어제처럼 굿나잇', 'UUID9484');
+insert into song (album_id, file_id, song_name, uuid) values (135, 4334, '안녕(Hi~)', 'UUID894');
+insert into song (album_id, file_id, song_name, uuid) values (432, 1353, 'Ah-Choo', 'UUID612');
+insert into song (album_id, file_id, song_name, uuid) values (354, 4681, 'WoW!', 'UUID8761');
