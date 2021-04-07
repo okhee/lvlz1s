0f1d52e99fae28799b43f646b7cd65a214a8bcfb
Jung Jongyoon <yoon97610@naver.com>
2021-04-07 22:08:18 +0900

    feat(SongFile): Add SongFile entity

---
+++ src/main/java/kr/co/okheeokey/domain/song/SongFile.java
@@ -0,0 +1,33 @@
+package kr.co.okheeokey.domain.song;
+
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Entity
+public class SongFile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "SONG_ID")
+    private Song song;
+
+    private String songFileName;
+
+    @Builder
+    public SongFile(String songFileName) {
+        this.songFileName = songFileName;
+    }
+
+    public void setSong(Song song) {
+        if(this.song != null){
+            this.song.getSongFile().remove(this);
+        }
+        this.song = song;
+        song.getSongFile().add(this);
+    }
+}
---
+++ src/main/java/kr/co/okheeokey/domain/song/SongFileRepository.java
@@ -0,0 +1,6 @@
+package kr.co.okheeokey.domain.song;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SongFileRepository extends JpaRepository<SongFile, Long> {
+}
