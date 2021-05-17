4d283e797c88cdb7fc8291a0bac1afe179d9d11f
Jung Jongyoon <yoon97610@naver.com>
2021-05-17 23:34:40 +0900

    test(SongFile): add SongFile controller test

--- src/test/java/kr/co/okheeokey/songfile/controller/SongFileControllerTest.java
+++ src/test/java/kr/co/okheeokey/songfile/controller/SongFileControllerTest.java
@@ -1,33 +1,44 @@
 package kr.co.okheeokey.songfile.controller;
 
+import kr.co.okheeokey.songfile.domain.AudioFileContentStore;
 import kr.co.okheeokey.songfile.domain.SongFile;
 import kr.co.okheeokey.songfile.domain.SongFileRepository;
 import kr.co.okheeokey.songfile.service.SongFileService;
-import lombok.NoArgsConstructor;
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.transaction.Transactional;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.InputStream;
+import java.sql.Blob;
 
 import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-@NoArgsConstructor
 public class SongFileControllerTest {
     @Autowired
     private MockMvc mvc;
+
+    @Autowired
+    private SongFileController songFileController;
 
     @Autowired
     private SongFileService songFileService;
@@ -35,18 +46,43 @@ public class SongFileControllerTest {
     @Autowired
     private SongFileRepository repository;
 
+    @Autowired
+    private AudioFileContentStore audioFileContentStore;
+
     @Test
+    @Transactional
     public void setContent() throws Exception {
-        FileInputStream audioStream = new FileInputStream(new File("src/main/resources/static/audio/11001.mp3"));
-        MockMultipartFile file = new MockMultipartFile("file", "11001.mp3", "multipart/form-data",
-                                    audioStream);
+        MockMultipartFile file1 = new MockMultipartFile("youre_not_alone_1m", "11001.mp3", "multipart/form-data",
+                    new FileInputStream(new File("src/main/resources/static/audio/11001.mp3")));
+        MockMultipartFile file2 = new MockMultipartFile("can_you_hear_me", "11002.mp3", "multipart/form-data",
+                    new FileInputStream(new File("src/main/resources/static/audio/11002.mp3")));
+
         Long id = 1L;
 
-        mvc.perform(MockMvcRequestBuilders.multipart("/songfiles/" + id)
-                    .file(file))
-                    .andExpect(status().isOk());
+        MultipartFile[] files = {file1, file2};
+        songFileController.setContent(id, files);
+
+//        mvc.perform(MockMvcRequestBuilders.multipart("/songfiles/" + id)
+//                    .file(file1)
+//                    .file(file2)
+//                    .contentType(MediaType.MULTIPART_FORM_DATA_VALUE))
+//                    .andExpect(status().isOk());
 
         SongFile songFile = repository.findById(id).get();
-        assertArrayEquals(file.getBytes(), songFile.getAudio());
+        assertArrayEquals(file1.getBytes(),
+                IOUtils.toByteArray(audioFileContentStore.getContent(songFile.getAudioList().get(0L))));
+        assertArrayEquals(file2.getBytes(),
+                IOUtils.toByteArray(audioFileContentStore.getContent(songFile.getAudioList().get(1L))));
+
+//        mvc.perform(MockMvcRequestBuilders.get("/songfiles/1?diff=0"))
+//                .andExpect(status().isOk())
+//                .andExpect(content().bytes(IOUtils.toByteArray(file1.getInputStream())));
+
+        assertArrayEquals(IOUtils.toByteArray(file1.getInputStream()), songFileService.getAudioFile(1L, 0L));
+
+        ResponseEntity<byte[]> responseEntity = songFileController.getAudioFile(id, 0L);
+        assertEquals(responseEntity.getStatusCode(), HttpStatus.OK);
+        assertArrayEquals(IOUtils.toByteArray(file1.getInputStream()), responseEntity.getBody());
+
     }
 }
