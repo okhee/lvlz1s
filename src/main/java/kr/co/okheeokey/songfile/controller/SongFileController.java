996cdea231c6115a59bcbc0210f4615409403fed
Jung Jongyoon <yoon97610@naver.com>
2021-05-17 23:33:59 +0900

    refactor(SongFile): use AudioFile to represent audio data

--- src/main/java/kr/co/okheeokey/songfile/controller/SongFileController.java
+++ src/main/java/kr/co/okheeokey/songfile/controller/SongFileController.java
@@ -2,25 +2,47 @@ package kr.co.okheeokey.songfile.controller;
 
 import kr.co.okheeokey.songfile.service.SongFileService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+
 @RequiredArgsConstructor
-@RestController
+@RestController("/songfiles")
 public class SongFileController {
     private final SongFileService songFileService;
 
-    @PostMapping("/songfiles/{id}")
-    public ResponseEntity<?> setContent(@PathVariable("id") Long id, @RequestParam("file") MultipartFile file) {
-        try {
-            songFileService.saveSongFile(id, file);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return new ResponseEntity<Object>(HttpStatus.NOT_FOUND);
+    @PostMapping(value = "/{id}", consumes = "multipart/form-data")
+    public ResponseEntity<?> setContent(@PathVariable("id") Long id, @RequestParam("files") MultipartFile[] files) {
+        Long difficulty = 0L;
+        for(MultipartFile file: files) {
+//            File f = new File("src/main/resources/static/audio/test", file.getOriginalFilename());
+//            new ClassPathResource("static/audio/test" + file.getOriginalFilename())
+//            try {
+//                f.createNewFile();
+//                file.transferTo(f);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+
+            try {
+                songFileService.saveSongFile(id, file, difficulty++);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return new ResponseEntity<Object>(HttpStatus.NOT_FOUND);
+            }
         }
+
         return new ResponseEntity<Object>(HttpStatus.OK);
     }
 
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<byte[]> getAudioFile(@PathVariable("id") Long id, @RequestParam("diff") Long difficulty) {
+        byte[] binaryAudioFile = songFileService.getAudioFile(id, difficulty);
+        return new ResponseEntity<>(binaryAudioFile, HttpStatus.OK);
+    }
 }
--- src/main/java/kr/co/okheeokey/songfile/service/SongFileService.java
+++ src/main/java/kr/co/okheeokey/songfile/service/SongFileService.java
@@ -1,24 +1,43 @@
 package kr.co.okheeokey.songfile.service;
 
-import kr.co.okheeokey.songfile.domain.SongFile;
-import kr.co.okheeokey.songfile.domain.SongFileRepository;
+import kr.co.okheeokey.songfile.domain.*;
+import kr.co.okheeokey.songfile.exception.NoAudioFileExistsException;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.transaction.Transactional;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.NoSuchElementException;
 
 @RequiredArgsConstructor
 @Service
-public class SongFileService {
+public class SongFileService{
     private final SongFileRepository songFileRepository;
+    private final AudioFileRepository audioFileRepository;
+    private final AudioFileContentStore audioFileContentStore;
 
     @Transactional
-    public void saveSongFile(Long songFileId, MultipartFile file) throws IOException, NoSuchElementException{
+    public void saveSongFile(Long songFileId, MultipartFile file, Long difficulty) throws IOException, NoSuchElementException, SQLException {
         SongFile songFile = songFileRepository.findById(songFileId).orElseThrow(NoSuchElementException::new);
-        songFile.setAudio(file.getBytes());
+
+        AudioFile audioFile = new AudioFile(difficulty);
+        audioFile.setSongFile(songFile);
+        audioFileRepository.save(audioFile);
+        audioFileContentStore.setContent(audioFile, file.getInputStream());
     }
 
+    public byte[] getAudioFile(Long songFileId, Long difficulty) throws NoAudioFileExistsException {
+        SongFile songFile = songFileRepository.findById(songFileId).orElseThrow(NoSuchElementException::new);
+        AudioFile audioFile = songFile.getAudio(difficulty);
+
+        try {
+            return IOUtils.toByteArray(audioFileContentStore.getContent(audioFile));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
