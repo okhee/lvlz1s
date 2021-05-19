463a390847b7ff904bed217cf1ceee8b4a4fa55d
Jung Jongyoon <yoon97610@naver.com>
2021-05-19 21:51:31 +0900

    resolve(SongFile): fix inappropriate URI matching and method parameter structure

--- src/main/java/kr/co/okheeokey/songfile/controller/SongFileController.java
+++ src/main/java/kr/co/okheeokey/songfile/controller/SongFileController.java
@@ -2,24 +2,26 @@ package kr.co.okheeokey.songfile.controller;
 
 import kr.co.okheeokey.songfile.service.SongFileService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
 
 @RequiredArgsConstructor
-@RestController("/songfiles")
+@RestController
+@RequestMapping("/songfiles")
 public class SongFileController {
     private final SongFileService songFileService;
 
-    @PostMapping(value = "/{id}", consumes = "multipart/form-data")
-    public ResponseEntity<?> setContent(@PathVariable("id") Long id, @RequestParam("files") MultipartFile[] files) {
-        Long difficulty = 0L;
-        for(MultipartFile file: files) {
+    @PostMapping(value = "/{id}")
+    public ResponseEntity<?> setContent(@RequestPart MultipartFile file,
+                                        @PathVariable("id") Long id,
+                                        @RequestParam(value = "diff", defaultValue = "0") Long difficulty) throws Exception{
+//        Long difficulty = 0L;
+//        for(MultipartFile file: files) {
 //            File f = new File("src/main/resources/static/audio/test", file.getOriginalFilename());
 //            new ClassPathResource("static/audio/test" + file.getOriginalFilename())
 //            try {
@@ -28,20 +30,17 @@ public class SongFileController {
 //            } catch (IOException e) {
 //                e.printStackTrace();
 //            }
+//        }
+        songFileService.saveAudioFile(id, file, difficulty);
 
-            try {
-                songFileService.saveSongFile(id, file, difficulty++);
-            } catch (Exception e) {
-                e.printStackTrace();
-                return new ResponseEntity<Object>(HttpStatus.NOT_FOUND);
-            }
-        }
-
-        return new ResponseEntity<Object>(HttpStatus.OK);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.LOCATION, "/songfiles/" + id + "?diff=" + difficulty);
+        return new ResponseEntity<>(headers, HttpStatus.CREATED);
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<byte[]> getAudioFile(@PathVariable("id") Long id, @RequestParam("diff") Long difficulty) {
+    public ResponseEntity<byte[]> getAudioFile(@PathVariable("id") Long id,
+                                               @RequestParam(value = "diff", defaultValue = "0") Long difficulty) throws IOException{
         byte[] binaryAudioFile = songFileService.getAudioFile(id, difficulty);
         return new ResponseEntity<>(binaryAudioFile, HttpStatus.OK);
     }
