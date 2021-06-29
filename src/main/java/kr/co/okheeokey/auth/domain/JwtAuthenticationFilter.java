40ca13073b3e07605d9624e4ac478e0f74d1e31a
Jung Jongyoon <yoon97610@naver.com>
2021-06-29 20:31:39 +0900

    change(JwtAuthFilter): change GenericFilterBean to OncePerRequestFilter

--- src/main/java/kr/co/okheeokey/auth/domain/JwtAuthenticationFilter.java
+++ src/main/java/kr/co/okheeokey/auth/domain/JwtAuthenticationFilter.java
@@ -1,39 +1,48 @@
 package kr.co.okheeokey.auth.domain;
 
 import io.jsonwebtoken.JwtException;
+import kr.co.okheeokey.auth.exception.JwtRuntimeException;
 import kr.co.okheeokey.auth.service.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
-import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Optional;
 
 @Slf4j
+@Component
 @RequiredArgsConstructor
-public class JwtAuthenticationFilter extends GenericFilterBean {
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final CustomUserDetailsService userDetailsService;
     private final JwtAuthTokenProvider jwtAuthTokenProvider;
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        log.info("JwtAuthFilter : doFilter() called");
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException, JwtRuntimeException {
+        log.info("JwtAuthFilter : doFilterInternal() called");
 
-        resolveToken((HttpServletRequest) request)
-                .map(jwtAuthTokenProvider::getSubject)
-                .map(userDetailsService::loadUserByUsername)
-                .map(userDetails -> new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities()))
-                .ifPresent(authentication -> SecurityContextHolder.getContext().setAuthentication(authentication));
+        // Check whether user has JwtToken that can prove his/her identity
+        // This is the only way to set Authentication on SecurityContext
+        resolveToken(request)
 
-        chain.doFilter(request, response);
+            // Check validity of token and return subject
+            .map(jwtAuthTokenProvider::getSubject)
+
+            // User is now authenticated, setting Authentication object
+            .map(userDetailsService::loadUserByUsername)
+            .map(userDetails -> new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities()))
+            .ifPresent(authentication -> SecurityContextHolder.getContext().setAuthentication(authentication));
+
+        filterChain.doFilter(request, response);
     }
 
     private Optional<String> resolveToken(HttpServletRequest request) {
