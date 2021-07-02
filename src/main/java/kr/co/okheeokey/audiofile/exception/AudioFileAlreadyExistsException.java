86b589b92d3418143a9c4395b7112598a47fa97f
Jung Jongyoon <yoon97610@naver.com>
2021-07-02 14:08:35 +0900

    add AudioFileException constructor with  message embedded

--- src/main/java/kr/co/okheeokey/audiofile/exception/AudioFileAlreadyExistsException.java
+++ src/main/java/kr/co/okheeokey/audiofile/exception/AudioFileAlreadyExistsException.java
@@ -4,4 +4,8 @@ public class AudioFileAlreadyExistsException extends RuntimeException {
     public AudioFileAlreadyExistsException(String message) {
         super(message);
     }
+    public AudioFileAlreadyExistsException(Long questionId, Long difficulty) {
+        super("Audio file already exists in Question id { " + questionId
+                + " }, difficulty { " + difficulty + " }; Use update request");
+    }
 }
--- src/main/java/kr/co/okheeokey/audiofile/exception/NoAudioFileExistsException.java
+++ src/main/java/kr/co/okheeokey/audiofile/exception/NoAudioFileExistsException.java
@@ -4,4 +4,9 @@ public class NoAudioFileExistsException extends RuntimeException {
     public NoAudioFileExistsException(String message) {
         super(message);
     }
+
+    public NoAudioFileExistsException(Long questionId, Long difficulty) {
+        super("Audio file does not exists in Question id { " + questionId
+                + " }, difficulty { " + difficulty + " }; Add audio file first");
+    }
 }
