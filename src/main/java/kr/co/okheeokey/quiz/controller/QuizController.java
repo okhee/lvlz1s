d1b4c51d608437e5a38f0d6c9aa02aa344af9da9
Jung Jongyoon <yoon97610@naver.com>
2021-07-04 17:13:57 +0900

    doc(Quiz): add javadoc to QuizController

--- src/main/java/kr/co/okheeokey/quiz/controller/QuizController.java
+++ src/main/java/kr/co/okheeokey/quiz/controller/QuizController.java
@@ -24,12 +24,37 @@ import java.util.NoSuchElementException;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
+/**
+ * Controller class that provides endpoint of "/quizs/**"
+ *
+ * @see QuizService
+ */
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/quizs")
 public class QuizController {
     private final QuizService quizService;
 
+    /**
+     * POST "/quizs" - Create new quiz instance
+     *
+     * <p> If user has previous ongoing quiz with same quiz set id,
+     * return such quiz instance instead </p>
+     *
+     * @param user [Authenticated]
+     * @param dto {@code quizSetId}, {@code questionNum}
+     *
+     * @return
+     * {@code 202 Accepted} <br>
+     * {@code Location}: "/quizs/{quizId}"
+     *
+     * @throws NoSuchElementException
+     *         If {@code quizSetId} is invalid
+     * @throws IllegalAccessException
+     *         If chosen {@code quizset} is neither ready-made nor created by {@code user}
+     * @throws IndexOutOfBoundsException
+     *         If {@code questionNum} is greater than size of {@code questionPool} of quizset
+     */
     @PostMapping
     public ResponseEntity<?> createQuiz(@AuthenticationPrincipal User user, @RequestBody QuizCreateDto dto)
             throws NoSuchElementException, IllegalAccessException, IndexOutOfBoundsException {
@@ -39,6 +64,19 @@ public class QuizController {
                 .location(URI.create("/quizs/" + quiz.getId())).build();
     }
 
+    /**
+     * GET "/quizs/{quizId}" - Get information of ongoing/finished quiz instance
+     *
+     * @param user [Authenticated]
+     * @param quizId Id of {@code quiz}
+     * @return
+     * {@code 200 OK} <br>
+     * {@link QuizStatusValues}
+     * @throws NoSuchElementException
+     *         If {@code quizId} is invalid
+     * @throws IllegalAccessException
+     *         If {@code user} is not owner of quiz
+     */
     @GetMapping("/{id}")
     public ResponseEntity<?> getQuizInfo(@AuthenticationPrincipal User user,
             @PathVariable("id") Long quizId) throws NoSuchElementException, IllegalAccessException {
@@ -50,6 +88,29 @@ public class QuizController {
         );
     }
 
+    /**
+     * GET "/quizs/{quizId}/q/{questionIndex}" - Get question info
+     *
+     * @param user [Authenticated]
+     * @param quizId Id of {@code quiz}
+     * @param questionIndex 1-based indexing; Index of question from {@code questionList}
+     *
+     * @return
+     * {@code 200 OK} <br>
+     * {@link QuestionInfoValues} - {@code encryptUuid}, {@code hintAvailable}, {@code nextHintCost}
+     * <p>{@code encryptUuid} - Encrypted UUID value; "/audiofiles/{encryptUuid}" </p>
+     * <p>{@code hintAvailable} - true or false; whether further hint audiofile exists</p>
+     * <p>{@code nextHintCost} - (1, 2, ...) or -1; Cost to grant additional hint, if none exists, -1</p>
+     *
+     * @throws IndexOutOfBoundsException
+     *         If {@code questionIndex} is greater than {@code questionNum}
+     * @throws NoSuchElementException
+     *         If ongoing quiz with id ({@code quizId}) does not exist
+     * @throws IllegalAccessException
+     *         If {@code user} is not owner of quiz
+     *
+     * @see AudioFileController#getAudioFile(String)
+     */
     @GetMapping("/{id}/q/{qid}")
     public ResponseEntity<?> getQuestion(@AuthenticationPrincipal User user,
             @PathVariable("id") Long quizId, @PathVariable("qid") Long questionIndex)
@@ -64,6 +125,24 @@ public class QuizController {
         );
     }
 
+    /**
+     * POST "/quizs/{quizId}/q/{questionIndex}" - Submit answer response of question
+     *
+     * @param user [Authenticated]
+     * @param quizId Id of {@code quiz}
+     * @param questionIndex 1-based indexing; Index of question from {@code questionList}
+     * @param submitDto
+     *        {@code responseSongId}: Id of {@code song}
+     * @return
+     * {@code 202 Accepted} <br>
+     *
+     * @throws NoSuchElementException
+     *         If ongoing quiz with id ({@code quizId}) does not exist or {@code songId} is invalid
+     * @throws IndexOutOfBoundsException
+     *         If {@code questionIndex} is greater than {@code questionNum}
+     * @throws IllegalAccessException
+     *         If {@code user} is not owner of quiz
+     */
     @PostMapping("/{id}/q/{qid}")
     public ResponseEntity<?> submitQuestion(@AuthenticationPrincipal User user, @PathVariable("id") Long quizId,
             @PathVariable("qid") Long questionIndex, @RequestBody QuestionSubmitDto submitDto)
@@ -75,6 +154,20 @@ public class QuizController {
         );
     }
 
+    /**
+     * POST "/quizs/{quizId}" - Finish and submit quiz instance
+     * <p>It accepts either every question is answered or not</p>
+     *
+     * @param user [Authenticated]
+     * @param quizId Id of {@code quiz}
+     * @return
+     * {@code 202 Accepted} <br>
+     *
+     * @throws NoSuchElementException
+     *         If ongoing quiz with id ({@code quizId}) does not exist
+     * @throws IllegalAccessException
+     *         If {@code user} is not owner of quiz
+     */
     @PostMapping("/{id}")
     public ResponseEntity<?> submitQuiz(@AuthenticationPrincipal User user, @PathVariable("id") Long quizId)
             throws NoSuchElementException, IllegalAccessException {
@@ -85,6 +178,21 @@ public class QuizController {
         );
     }
 
+    /**
+     * DELETE "/quizs/{quizId}" - Give up ongoing quiz instance
+     * <p>It has no effect on finished quiz instance.</p>
+     *
+     * @param user [Authenticated]
+     * @param quizId Id of {@code quiz}
+     *
+     * @return
+     * {@code 204 No Content} <br>
+     *
+     * @throws NoSuchElementException
+     *         If ongoing quiz with id ({@code quizId}) does not exist
+     * @throws IllegalAccessException
+     *         If {@code user} is not owner of quiz
+     */
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<?> giveUpQuiz(@AuthenticationPrincipal User user, @PathVariable("id") Long quizId)
             throws NoSuchElementException, IllegalAccessException {
