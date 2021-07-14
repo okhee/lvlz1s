6065d1ff46aa26c51f2f7104bbd54594824cf75f
starShade8 <yoon97610@naver.com>
2021-07-14 17:08:47 +0900

    feat(AudioFileService): update file format identification by using Apache Tika

--- src/main/java/kr/co/okheeokey/audiofile/service/AudioFileService.java
+++ src/main/java/kr/co/okheeokey/audiofile/service/AudioFileService.java
@@ -12,13 +12,14 @@ import kr.co.okheeokey.question.domain.QuestionRepository;
 import kr.co.okheeokey.question.vo.AudioFileValues;
 import kr.co.okheeokey.util.CryptoUtils;
 import lombok.RequiredArgsConstructor;
+import org.apache.tika.Tika;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import org.springframework.transaction.annotation.Transactional;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -72,20 +73,12 @@ public class AudioFileService {
     }
 
     private String verifyMultipartFile(MultipartFile file) throws IOException {
-        byte[] mp3MagicNumber = new byte[]{0x49, 0x44, 0x33};
-        byte[] flacMagicNumber = new byte[]{0x66, 0x4C, 0x61, 0x43};
-        byte[] buffer = new byte[4];
+        String mediaType = new Tika().detect(file.getInputStream());
 
-        if (file.getInputStream().read(buffer) < 0)
-            throw new InvalidFormatException("Uploaded file is not mp3 nor flac");
+        if (!mediaType.contains("audio/"))
+            throw new InvalidFormatException("The uploaded file is not an audio file");
 
-        if (Arrays.equals(mp3MagicNumber, Arrays.copyOfRange(buffer, 0, mp3MagicNumber.length)))
-            return "audio/mpeg";
-
-        if (Arrays.equals(flacMagicNumber, Arrays.copyOfRange(buffer, 0, flacMagicNumber.length)))
-            return "audio/flac";
-
-        throw new InvalidFormatException("Uploaded file is not mp3 nor flac");
+        return mediaType;
     }
 
 }
